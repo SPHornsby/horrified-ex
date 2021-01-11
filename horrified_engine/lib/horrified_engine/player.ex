d383366b784ce6680444e515a66fa8d9db96e029
SPHornsby <sean.hornsby@gmail.com>
2021-01-10 16:04:42 -0800

    More fields.

--- horrified_engine/lib/horrified_engine/player.ex
+++ horrified_engine/lib/horrified_engine/player.ex
@@ -1,10 +1,15 @@
 defmodule HorrifiedEngine.Player do
   alias __MODULE__
 
-  @enforce_keys [:name]
-  defstruct [:name]
+  @enforce_keys [:name, :hero, :status, :items, :perks]
+  defstruct [:name, :hero, :items, :perks, :status]
 
-  def new(name), do:
-    %Player{name: name}
+  def new(name) do
+    hero = ""
+    items = MapSet.new()
+    perks = MapSet.new()
+    
+    %Player{name: name, hero: hero, status: :in_play, items: items, perks: perks}
+  end
 
 end
