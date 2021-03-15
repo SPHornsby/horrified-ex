983cff00e5e28e7b06053675feb740fa5d0dc4a4
SPHornsby <sean.hornsby@gmail.com>
2021-03-15 07:59:44 -0700

    m card

--- horrified_engine/lib/horrified_engine/monster_card.ex
+++ horrified_engine/lib/horrified_engine/monster_card.ex
@@ -2,17 +2,13 @@ defmodule HorrifiedEngine.MonsterCard do
   alias HorrifiedEngine.{Board, MonsterCard, Villager}
 
   @enforce_keys [:name, :text]
-  defstruct [:name, :text, :item_tokens, :monsters, :movement, :dice, :action
-]
+  defstruct [:name, :text, :item_tokens, :monsters, :movement, :dice, :action]
 
   def new(name, text, item_tokens, monsters, movement, dice), do:
     new(name, text, item_tokens, monsters, movement, dice, [])
 
-  def new(name, text, item_tokens, monsters, movement, dice, action
-  ), do:
-    %MonsterCard{name: name, text: text, item_tokens: item_tokens, monsters: monsters, movement: movement, dice: dice, action
-    : action
-  }
+  def new(name, text, item_tokens, monsters, movement, dice, action), do:
+    %MonsterCard{name: name, text: text, item_tokens: item_tokens, monsters: monsters, movement: movement, dice: dice, action: action}
 
   def get_deck() do
     [
@@ -22,10 +18,10 @@ defmodule HorrifiedEngine.MonsterCard do
           ["frankenstein", "bride", "frenzy"],
           1,
           2, 
-          fn(board) ->
-            beth = Villager.get_villager("Elizabeth")
-            Board.place_villager_and_update_board(board, "mansion", beth)
-          end
+            fn(board) ->
+              beth = Villager.get_villager("Elizabeth")
+              Board.place_villager_and_update_board(board, "mansion", beth)
+            end
       ),
       new("Egyptian Expert", "Place Prof. Pearson at the Cave", 3, ['mummy', 'frenzy'], 2, 2, [])
     ] |> Enum.shuffle
